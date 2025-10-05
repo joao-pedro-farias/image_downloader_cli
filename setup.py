1e9135356807871a3e20dd16811afa7819c57980
joao-pedro-farias <joaopedro.esf@gmail.com>
2025-10-05 15:23:20 -0300

    mudei a estrutura

--- setup.py
+++ setup.py
@@ -1,31 +1,5 @@
-from setuptools import setup, find_packages
+# setup.py (Versão Simplificada)
+from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+setup()
 
-with open("requirements.txt", "r", encoding="utf-8") as fh:
-    requirements = fh.read().splitlines()
-
-setup(
-    name="imgdown",
-    version="1.0.0",
-    author="Seu Nome",
-    author_email="seu@email.com",
-    description="Async image downloader CLI tool",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/seuusuario/async-image-downloader",
-    packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.7",
-    install_requires=requirements,
-    entry_points={
-        "console_scripts": [
-            "imgdown=imgdown.cli:main",
-        ],
-    },
-)
